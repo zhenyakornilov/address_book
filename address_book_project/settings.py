0b4e2e0d6e5732a55b18352e30bde024f95b7baf
Yevhen Kornilov <zhenyakornilov98@gmail.com>
2021-11-21 21:24:55 +0200

    change settings.py

--- address_book_project/settings.py
+++ address_book_project/settings.py
@@ -127,7 +127,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media').replace('\\', '/')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
